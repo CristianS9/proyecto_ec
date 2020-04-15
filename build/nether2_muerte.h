6951d2516696eded983c84b8fdd700e2d398fc11
ikerRAD <ikerbueno30@gmail.com>
2020-04-15 10:25:32 -0700

    fondos crashean v2

--- build/nether2_muerte.h
+++ build/nether2_muerte.h
@@ -8,7 +8,7 @@
 //	+ bitmap not compressed
 //	Total size: 97792 = 97792
 //
-//	Time-stamp: 2020-04-15, 10:15:36
+//	Time-stamp: 2020-04-15, 10:24:40
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
 //	( http://www.coranac.com/projects/#grit )
 //
--- include/fondos.h
+++ include/fondos.h
@@ -3,9 +3,24 @@ extern void initFondos();
 
 /* Para cada imagen que se quiera llevar a pantalla hay que hacer una de estas funciones. */
 
-extern void fondo1();
-extern void fondo2();
-extern void fondo3();
+extern void over1();
+extern void over2();
+extern void over3();
+extern void over1Muerte();
+extern void over2Muerte();
+extern void over3Muerte();
+extern void cielo1();
+extern void cielo2();
+extern void cielo3();
+extern void cielo1Muerte();
+extern void cielo2Muerte();
+extern void cielo3Muerte();
+extern void nether1();
+extern void nether2();
+extern void nether3();
+extern void nether1Muerte();
+extern void nether2Muerte();
+extern void nether3Muerte();
 
 
 /* Definir el sistema de vídeo */
