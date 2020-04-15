9a01cd96f168042533ade71c48e208cf3d41ca6f
CristianS9 <CristianS9@hotmail.es>
2020-04-15 18:03:15 +0200

    slime base funciona

--- build/fondo1.h
+++ build/fondo1.h
@@ -8,7 +8,7 @@
 //	+ bitmap not compressed
 //	Total size: 97792 = 97792
 //
-//	Time-stamp: 2020-04-15, 17:24:45
+//	Time-stamp: 2020-04-15, 18:02:31
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
 //	( http://www.coranac.com/projects/#grit )
 //
